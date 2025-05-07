2e3d60ebfcccb07fbdd0ffd8c4498c28d7b51c61
Erick David Mendoza Velasco <erick_tec@outlook.com>
2025-05-06 18:51:56 -0600

    local playlist added

--- pomodoro/src/components/musicAlbumCard/MusicAlbumCard.tsx
+++ pomodoro/src/components/musicAlbumCard/MusicAlbumCard.tsx
@@ -1,15 +1,21 @@
 import "./musicAlbumCard.css";
 
+export type MusicSources = "native" | "spotify";
+
 type Props = {
     title: string;
     image: string;
-    source: "native" | "spotify";
-    albumLink?: string;
+    source: MusicSources;
+    link: string;
+    onClick?: (source: MusicSources, link: string) => void;
 };
 
-const MusicAlbumCard = ({ title, image, source, albumLink }: Props) => {
+const MusicAlbumCard = ({ title, image, source, link, onClick }: Props) => {
     return (
-        <div className="musicAlbumCard">
+        <div
+            className="musicAlbumCard"
+            onClick={onClick ? () => onClick(source, link) : undefined}
+        >
             <img
                 className="musicAlbumCard__image"
                 src={image}
--- pomodoro/src/components/musicPlayer/MusicPlayer.tsx
+++ pomodoro/src/components/musicPlayer/MusicPlayer.tsx
@@ -1,27 +1,61 @@
+import { useState } from "react";
 import Carousel from "../carousel/CarouselIndex";
-import MusicAlbumCard from "../musicAlbumCard/MusicAlbumCard";
+import MusicAlbumCard, { MusicSources } from "../musicAlbumCard/MusicAlbumCard";
 import "./musicPlayer.css";
 
 type Props = {
     className?: string;
 };
 
+const albums: Array<{
+    title: string;
+    image: string;
+    source: MusicSources;
+    link: string;
+}> = [
+    {
+        title: "Unreal Engine Playlist",
+        image: "https://i.scdn.co/image/ab6761610000101fc9e097800c03fd3fb38c05bc",
+        source: "spotify",
+        link: "https://open.spotify.com/embed/artist/5n0h7PGhxq6dC4LCWTEcMA?utm_source=generator&theme=0",
+    },
+    {
+        title: "Test",
+        image: "https://image-cdn-fa.spotifycdn.com/image/ab67706c0000da843af3ae28da6da6a9dd9bb658",
+        source: "spotify",
+        link: "https://open.spotify.com/embed/playlist/78YRowPpgNrQurjR2Wx4um?utm_source=generator",
+    },
+];
+
 const MusicPlayer = ({ className }: Props) => {
+    const [playlistLink, setPlaylistLink] = useState(albums[0].link);
+
+    const onClickMusicCard = (source: MusicSources, link: string) => {
+        if (source === "spotify") {
+            setPlaylistLink(link);
+        }
+    };
+
     return (
         <section className={`musicPlayer ${className ?? ""}`}>
             <Carousel className="musicPlayer__carousel" itemSizeInPixels={250}>
-                <Carousel.Item>
-                    <MusicAlbumCard
-                        title="Test"
-                        image="https://picsum.photos/200"
-                        source="spotify"
-                    />
-                </Carousel.Item>
+                {albums.map((album) => (
+                    <Carousel.Item>
+                        <MusicAlbumCard
+                            onClick={onClickMusicCard}
+                            title={album.title}
+                            image={album.image}
+                            source={album.source}
+                            link={album.link}
+                        />
+                    </Carousel.Item>
+                ))}
             </Carousel>
+
             <iframe
                 className="musicPlayer__player"
                 title="spotify"
-                src="https://open.spotify.com/embed/playlist/6phYndBIC4DIqefH1CcUsT?utm_source=generator&theme=0"
+                src={playlistLink}
                 allow="autoplay;
       clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                 loading="lazy"
